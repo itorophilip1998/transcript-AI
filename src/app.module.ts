c196ff8935fe0c55f9bf9cd8d4e7e33063d2b63e
Itorophilip1998 <itorophilip1998@gmail.com>
2025-01-09 23:32:20 +0100

    UPdate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TranscribeModule } from './transcribe/transcribe.module';
 
 @Module({
-  imports: [TranscribeModule], 
+  imports: [TranscribeModule],
 })
 export class AppModule {}
